dfdcc248ec01878d405e5f8507dd3d7a6b0a2b0c
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2022-08-14 15:59:06 -0300

    add dateFormatFirebase function.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,20 @@
+import { FirestoreDataConverter } from "firebase/firestore";
+
 export const formatDate = (date: string) => {
   const newDate = date.split(" ");
 
   return `${newDate[0]}, ${newDate[2]} ${newDate[1]}, ${newDate[3]}`;
 };
+
+export function dateFormatFirebase(timestamp: any) {
+  if (timestamp) {
+    const date = new Date(timestamp.toDate());
+
+    const day = date.getDate();
+    const month = `${date.toDateString().split(" ")[1]} ${
+      date.toDateString().split(" ")[3]
+    }`;
+
+    return `${day} ${month}`;
+  }
+}
